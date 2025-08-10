c9757edb289952abbc8610abf28e4597f2bbf3b9
Mateus <mateusmarques18icm@gmail.com>
2025-08-10 02:08:15 -0300

    Commit inicial do projeto spring boot

---
+++ src/main/java/br/com/jzsports/tournament_control/TournamentControlApplication.java
@@ -0,0 +1,13 @@
+package br.com.jzsports.tournament_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournamentControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TournamentControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jzsports/tournament_control/TournamentControlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jzsports.tournament_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournamentControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
