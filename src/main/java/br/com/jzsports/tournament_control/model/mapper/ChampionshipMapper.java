9d9728a7cea2183b807c05642adc33022623d2db
Mateus <mateusmarques18icm@gmail.com>
2025-08-17 21:49:08 -0300

    refact: refat classes mapper para suportar novo mapeamento entre as mesmas

--- src/main/java/br/com/jzsports/tournament_control/model/mapper/ChampionshipMapper.java
+++ src/main/java/br/com/jzsports/tournament_control/model/mapper/ChampionshipMapper.java
@@ -4,9 +4,12 @@ import br.com.jzsports.tournament_control.model.dto.ChampionshipDTO;
 import br.com.jzsports.tournament_control.model.entity.Championship;
 import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring", uses = {TeamMapper.class, MatchMapper.class}, builder = @Builder(disableBuilder = true))
+@Mapper(componentModel = "spring", uses = {PlayerMapper.class, TeamMapper.class}, builder = @Builder(disableBuilder = true))
 public interface ChampionshipMapper {
+    @Mapping(target = "createdBy", qualifiedByName = "toSimpleDto")
     ChampionshipDTO toDto(Championship championship);
     Championship toEntity(ChampionshipDTO championshipDTO);
+
 }
--- src/main/java/br/com/jzsports/tournament_control/model/mapper/PlayerMapper.java
+++ src/main/java/br/com/jzsports/tournament_control/model/mapper/PlayerMapper.java
@@ -1,16 +1,18 @@
 package br.com.jzsports.tournament_control.model.mapper;
 import br.com.jzsports.tournament_control.model.dto.PlayerDTO;
+import br.com.jzsports.tournament_control.model.dto.PlayerSimpleDTO;
 import br.com.jzsports.tournament_control.model.entity.Player;
 import br.com.jzsports.tournament_control.model.mapper.config.IgnoreImmutableConfig;
-import org.mapstruct.Builder;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring", uses = {TeamMapper.class}, builder = @Builder(disableBuilder = true), config = IgnoreImmutableConfig.class)
 public interface PlayerMapper {
 
     PlayerDTO toDto(Player player);
-    Player toEntity(PlayerDTO player);
+    Player toEntity(PlayerDTO playerDTO);
     void updatePlayer(PlayerDTO playerDTO, @MappingTarget Player player);
+    // Versão simplificada para usar no ChampionshipDTO
+    @Named("toSimpleDto")
+    PlayerSimpleDTO toSimpleDto(Player player);
 
 }
