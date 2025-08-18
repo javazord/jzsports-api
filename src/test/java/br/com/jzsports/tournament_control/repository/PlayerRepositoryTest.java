960707d0589fc72d23a8d4d783643c0bf8419e14
Mateus <mateusmarques18icm@gmail.com>
2025-08-17 21:51:27 -0300

    test: configuração para testes; impl teste de player e team, repository e service, e repository respectivamente

---
+++ src/test/java/br/com/jzsports/tournament_control/repository/PlayerRepositoryTest.java
@@ -0,0 +1,65 @@
+package br.com.jzsports.tournament_control.repository;
+
+import br.com.jzsports.tournament_control.model.entity.Player;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@ActiveProfiles("test")
+class PlayerRepositoryTest {
+
+    @Autowired
+    PlayerRepository playerRepository;
+
+    private Player player;
+
+    @BeforeEach
+    void setUp() {
+        player = createPlayer();
+        this.playerRepository.save(player);
+    }
+
+    @Test
+    @DisplayName("Should exists Player by email or nickname from database")
+    void existsPlayerByEmailOrNicknameCase1() {
+        boolean existsPlayerByEmailOrNickname = this.playerRepository.existsPlayerByEmailOrNickname( player.getEmail(), player.getNickname());
+
+        assertThat(existsPlayerByEmailOrNickname).isTrue();
+
+    }
+
+    @Test
+    @DisplayName("Should not exists Player by email or nickname from database")
+    void existsPlayerByEmailOrNicknameCase2() {
+        String email = "teste@gmail.com";
+        String nickname = "teste";
+        boolean existsPlayerByEmailOrNickname = this.playerRepository.existsPlayerByEmailOrNickname( email, nickname);
+
+        assertThat(existsPlayerByEmailOrNickname).isFalse();
+
+    }
+
+    private Player createPlayer() {
+        return new Player(
+                null,
+                "Mateus",
+                "javazord",
+                "123@",
+                "mateus@gmail.com",
+                "undefined",
+                LocalDateTime.now(),
+                new ArrayList<>(),
+                new ArrayList<>()
+        );
+    }
+
+
+
+}
---
+++ src/test/java/br/com/jzsports/tournament_control/repository/TeamRepositoryTest.java
@@ -0,0 +1,83 @@
+package br.com.jzsports.tournament_control.repository;
+
+import br.com.jzsports.tournament_control.model.entity.Championship;
+import br.com.jzsports.tournament_control.model.entity.Player;
+import br.com.jzsports.tournament_control.model.entity.Team;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@ActiveProfiles("test")
+class TeamRepositoryTest {
+
+    @Autowired
+    private TeamRepository teamRepository;
+
+    @Autowired
+    private ChampionshipRepository championshipRepository;
+
+    @Autowired
+    private PlayerRepository playerRepository;
+
+    @BeforeEach
+    void setUp() {
+        // cria jogador
+        Player player = createPlayer();
+        playerRepository.save(player);
+
+        // cria time
+        Team team = new Team();
+        team.setName("Team A");
+        teamRepository.save(team);
+
+        // cria campeonato
+        Championship championship = new Championship();
+        championship.setName("Championship 1");
+        championship.setCreatedBy(player);
+        championship.setTeamsList(new ArrayList<>());
+        championship.getTeamsList().add(team);
+        championshipRepository.save(championship);
+
+    }
+
+    @Test
+    @DisplayName("Deve retornar true se existir time com o nome em um campeonato")
+    void existsByNameAndChampionshipList_IdCase1() {
+        boolean exists = teamRepository.existsByNameAndChampionshipList_Id("Team A", 1L);
+
+        assertThat(exists).isTrue();
+    }
+
+    @Test
+    @DisplayName("Deve retornar false se não existir time com o nome em um campeonato")
+    void existsByNameAndChampionshipList_IdCase2() {
+        boolean exists = teamRepository.existsByNameAndChampionshipList_Id("Team B", 1L);
+
+        assertThat(exists).isFalse();
+    }
+
+    private Player createPlayer() {
+        return new Player(
+                null,
+                "Mateus",
+                "javazord",
+                "123@",
+                "mateus@gmail.com",
+                "undefined",
+                LocalDateTime.now(),
+                new ArrayList<>(),
+                new ArrayList<>()
+        );
+    }
+}
