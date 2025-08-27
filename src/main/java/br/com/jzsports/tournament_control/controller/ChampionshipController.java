cfb8dd8027e9c3ff07cf635b4bb03c4c253087ba
Mateus Bastos Marques <mateus.marques@AGU.GOV.BR>
2025-08-27 10:28:33 -0300

    refact: alteração no request mapping de championship

--- src/main/java/br/com/jzsports/tournament_control/controller/ChampionshipController.java
+++ src/main/java/br/com/jzsports/tournament_control/controller/ChampionshipController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping
+@RequestMapping("/api/championships")
 public class ChampionshipController {
 
     private ChampionshipService championshipService;
